9dc87e1c128ec0290b55981b3fac50d915e06683
UnderKoen <koenvanstaveren@hotmail.com>
2020-11-13 11:38:26 +0100

    Upload design

--- Designer/ViewModel/AddDesignModel.cs
+++ Designer/ViewModel/AddDesignModel.cs
@@ -1,37 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Designer.Model;
 
 namespace Designer.ViewModel {
+    public class DefaultCommand : ICommand {
+        private readonly Action _action;
+        public bool CanExecute(object parameter) => true;
+        public void Execute(object parameter) => _action?.Invoke();
+
+        public DefaultCommand(Action action) { _action = action; }
+
+        public event EventHandler CanExecuteChanged;
+    }
+    
     public class AddDesignModel : INotifyPropertyChanged {
         public event PropertyChangedEventHandler PropertyChanged;
         
         public List<Room> Rooms { get; set; } = new List<Room>();
         public Room Selected { get; set; }
+        public string Name { get; set; }
+        public ICommand Submit { get ; set; }
 
-        public string Name { get => "Hey"; set => Console.WriteLine(value); }
+        private RoomDesignContext _context;
 
         public AddDesignModel() {
-            Rooms.Add(new Room() {
-                Name = "T1.07", Length = 10, Width = 10, RoomId = 1
-            });
-            Rooms.Add(new Room() {
-                Name = "T1.08", Length = 10, Width = 10, RoomId = 5
-            });
-            Rooms.Add(new Room() {
-                Name = "T1.09", Length = 10, Width = 10, RoomId = 2
-            });
-            Rooms.Add(new Room() {
-                Name = "T1.10", Length = 10, Width = 10, RoomId = 9
-            });
-            Rooms.Add(new Room() {
-                Name = "T1.11", Length = 10, Width = 10, RoomId = 3
-            });
+            _context = new RoomDesignContext();
+            Rooms = _context.Rooms.ToList();
+            Submit = new DefaultCommand(() => _submit());
         }
 
         private void OnPropertyChanged(string propertyName = "") {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private async Task _submit() {
+            Design design = new Design(Name, Selected, new List<ProductPlacement>());
+            await _context.Designs.AddAsync(design);
+            await _context.SaveChangesAsync();
+            Console.WriteLine("Added");
         }
     }
 }
