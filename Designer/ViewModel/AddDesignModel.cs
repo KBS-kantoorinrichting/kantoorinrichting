bf176e3d82f141594208be2e0f14f7cc1d5fbb9c
UnderKoen <koenvanstaveren@hotmail.com>
2020-11-13 14:56:31 +0100

    Disabled submit when not possible

--- Designer/ViewModel/AddDesignModel.cs
+++ Designer/ViewModel/AddDesignModel.cs
@@ -7,25 +7,51 @@ using System.Windows.Input;
 using Designer.Model;
 
 namespace Designer.ViewModel {
-    
     //Command wat een actie uitvoert geen async
     public class DefaultCommand : ICommand {
         private readonly Action _action;
-        public bool CanExecute(object parameter) => true;
+        private bool _disabled;
+        public bool Disabled {
+            get => _disabled;
+            set {
+                _disabled = value;
+                CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+            }
+        }
+
+        public bool CanExecute(object parameter) => !Disabled;
         public void Execute(object parameter) => _action?.Invoke();
 
-        public DefaultCommand(Action action) { _action = action; }
+        public DefaultCommand(Action action, bool disabled = false) {
+            _action = action;
+            Disabled = disabled;
+        }
 
         public event EventHandler CanExecuteChanged;
     }
-    
+
     public class AddDesignModel : INotifyPropertyChanged {
         public event PropertyChangedEventHandler PropertyChanged;
-        
+
         public List<Room> Rooms { get; set; }
-        public Room Selected { get; set; }
-        public string Name { get; set; }
-        public ICommand Submit { get ; set; }
+        private Room _selected;
+        public Room Selected {
+            get => _selected;
+            set {
+                _selected = value;
+                Submit.Disabled = _selected == null || Name == null;
+            }
+        }
+
+        private string _name;
+        public string Name {
+            get => _name;
+            set {
+                _name = value;
+                Submit.Disabled = _selected == null || Name == null;
+            }
+        }
+        public DefaultCommand Submit { get; set; }
         public string Error { get; set; }
 
         //Wordt aangeroepen wanneer het design toegevoegd is
@@ -33,7 +59,7 @@ namespace Designer.ViewModel {
 
         public AddDesignModel() {
             Rooms = LoadRooms();
-            Submit = new DefaultCommand(AddDesign);
+            Submit = new DefaultCommand(AddDesign, true);
         }
 
         private void OnPropertyChanged(string propertyName = "") {
@@ -45,7 +71,7 @@ namespace Designer.ViewModel {
                 ShowError("Vul eerst alles in.");
                 return;
             }
-            
+
             Design design = CreateDesign(Name, Selected);
             design = SaveDesign(design);
             DesignAdded?.Invoke(this, new DesignAddedArgs(design));
@@ -58,13 +84,12 @@ namespace Designer.ViewModel {
                 t => {
                     Error = "";
                     OnPropertyChanged();
-                });
+                }
+            );
         }
 
-        public static List<Room> LoadRooms() {
-            return RoomDesignContext.Instance.Rooms.ToList();
-        }
-        
+        public static List<Room> LoadRooms() { return RoomDesignContext.Instance.Rooms.ToList(); }
+
         public static Design CreateDesign(string name, Room room) {
             return new Design(name, room, new List<ProductPlacement>());
         }
