a63ecf9e923e04a60b79df0fd74ace8269038e93
Kasper Vegte, van der <s1148087@student.windesheim.nl>
2020-11-13 10:40:06 +0100

    Moved mainwindow to root and changed naming of views

--- Designer/MainWindow.xaml.cs
+++ Designer/MainWindow.xaml.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Designer.View;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,10 +9,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Designer.View
+namespace Designer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
