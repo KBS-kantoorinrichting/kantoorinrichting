ecf72187c95e33e753792dc38799f4fe783fe444
Julien2212 <58032666+Julien2212@users.noreply.github.com>
2020-11-27 15:57:57 +0100

    ja

--- Designer/ViewModel/RoomEditorViewModel.cs
+++ Designer/ViewModel/RoomEditorViewModel.cs
@@ -4,17 +4,45 @@ using Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
 using System.Text;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Shapes;
 
 namespace Designer.ViewModel
 {
-    public class RoomEditorViewModel
+    public class RoomEditorViewModel : INotifyPropertyChanged
     {
         public string Name { get; set; }
+        public List<Line> GridLines = new List<Line>();
+        public Canvas Editor { get; set; }
+        public Border CanvasBorder { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
+        public ArgumentCommand<MouseEventArgs> MouseOverCommand { get; set; }
         public BasicCommand Submit { get; set; }
+        private double CanvasHeight = 500;
+        private double CanvasWidth = 1280;
 
-
+        public RoomEditorViewModel()
+        {
+            MouseOverCommand = new ArgumentCommand<MouseEventArgs>(e => MouseMove(e.OriginalSource, e));
+            Editor = new Canvas();
+            Reload();
+        }
+        public void Reload()
+        { // Reload de items zodat de juiste te zien zijn
+            Editor.Children.Clear();
+            DrawGrid();
+            OnPropertyChanged();
+        }
+        private void OnPropertyChanged(string propertyName = "")
+        {
+            // herlaad de hele pagina
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         public void SubmitRoom()
         {
             /*//if (SaveRoom(Name) != null)
@@ -29,10 +57,41 @@ namespace Designer.ViewModel
             popuperror.ShowDialog();*/
         }
 
-       
+        public void DrawGrid()
+        {
+
+            int LengthPerSquare = (int)CanvasWidth / 25;
+
+            for (int i = 1; i <= LengthPerSquare; i++)
+            {
+                Line line = new Line();
+                line.Stroke = System.Windows.Media.Brushes.Black;
+                line.Y1 = 0;
+                line.Y2 = 700;
+                line.X1 = (i * 25);
+                line.X2 = i * 25;
+                GridLines.Add(line);
+            }
+            for (int i = 0; i <= LengthPerSquare; i++)
+            {
+                Line line = new Line();
+                line.Stroke = System.Windows.Media.Brushes.Black;
+                line.X1 = 0;
+                line.X2 = 1280;
+                line.Y1 = (i * 25);
+                line.Y2 = i * 25;
+                GridLines.Add(line);
+            }
+            foreach (Line line in GridLines)
+            {
+                Editor.Children.Add(line);
+            }
+        }
+        public void MouseMove(object sender, MouseEventArgs e)
+        {
+            Editor.Background = System.Windows.Media.Brushes.Black;
+        }
+
     }
-
-
-        
 }
 
