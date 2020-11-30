17c2e1c8b293b2c5f745851fa80b9572d3a31445
SenneChocomel <senne1320@gmail.com>
2020-11-30 17:11:03 +0100

    selectie van hokjes en lijntjes trekken gemaakt

--- Designer/ViewModel/RoomEditorViewModel.cs
+++ Designer/ViewModel/RoomEditorViewModel.cs
@@ -20,6 +20,7 @@ namespace Designer.ViewModel
         public string Name { get; set; }
         public List<Line> GridLines = new List<Line>();
         public List<System.Windows.Point> Points = new List<System.Windows.Point>();
+        public List<System.Windows.Point> SelectedPoints = new List<System.Windows.Point>();
         public Dictionary<System.Windows.Point, System.Windows.Shapes.Rectangle> RectangleDictionary = new Dictionary<System.Windows.Point, System.Windows.Shapes.Rectangle>();
         public Canvas Editor { get; set; }
         public System.Windows.Shapes.Rectangle HoveredRectangle = new System.Windows.Shapes.Rectangle();
@@ -35,6 +36,7 @@ namespace Designer.ViewModel
 
         public RoomEditorViewModel()
         {
+            LastSelected.Y = 1000000;
             MouseOverCommand = new ArgumentCommand<MouseEventArgs>(e => MouseMove(e.OriginalSource, e));
             MouseDownCommand = new ArgumentCommand<MouseButtonEventArgs>(e => MouseClick(e.OriginalSource, e));
             Editor = new Canvas();
@@ -150,26 +152,113 @@ namespace Designer.ViewModel
             int x = Convert.ToInt32(e.GetPosition(Editor).X - (e.GetPosition(Editor).X % 25));
             //int x = Convert.ToInt32(e.GetPosition(Editor).X);
 
+            // controleer of de linkermuisknop ingedrukt werdt
             if (e.LeftButton == MouseButtonState.Pressed)
             {
-                if (LastSelected != null)
-                {
-                    var lastselected = RectangleDictionary[LastSelected];
-                    var currentpoint = new System.Windows.Point(y, x);
 
-                    if (lastselected.Equals(currentpoint))
+
+                // het momentele punt instellen
+                var currentpoint = new System.Windows.Point(y, x);
+
+                // als er een vorig item geselcteerd is
+                if (!LastSelected.Y.Equals(1000000))
+                {
+                    // het vorig geselecteerde punt instellen
+                    var lastselected = RectangleDictionary[LastSelected];
+                    
+                    // als het punt al eerder geselecteerd is
+                    if (LastSelected.Equals(currentpoint) || SelectedPoints.Contains(currentpoint))
                     {
+                        // maakt hokje weer wit
                         RectangleDictionary[currentpoint].Fill = System.Windows.Media.Brushes.White;
+                        // verwijderd hokje uit de lijst
+                        SelectedPoints.Remove(currentpoint);
                     }
-                    RectangleDictionary[currentpoint].Fill = System.Windows.Media.Brushes.DarkMagenta;
-                    LastSelected = currentpoint;
+                    else // als het punt nog niet eerder geselecteerd is
+                    {
+
+
+                        // maakt alle tussenhokje magenta maar voegt ze niet toe aan de lijst
+
+                        // kijk of de coord gelijk zijn met de x of de y
+                        if (LastSelected.X == currentpoint.X)
+                        {
+
+                            // als het getal negatief is moet er naar boven worden getekend, positief is onder
+                            if ((LastSelected.X - currentpoint.X < 0))
+                            {
+                                for (int i = (int)LastSelected.Y; i < currentpoint.Y; i -= 25)
+                                {
+                                    LastSelected.Y = i;
+                                    RectangleDictionary[LastSelected].Fill = System.Windows.Media.Brushes.DarkMagenta;
+                                }
+                            }
+                            else
+                            {
+                                for (int i = (int)LastSelected.Y; i < currentpoint.Y; i += 25)
+                                {
+                                    LastSelected.Y = i;
+                                    RectangleDictionary[LastSelected].Fill = System.Windows.Media.Brushes.DarkMagenta;
+                                }
+                            }
+
+                            // voegt hoekje toe aan lijst
+                            SelectedPoints.Add(currentpoint);
+                            // maakt hokje magenta
+                            RectangleDictionary[currentpoint].Fill = System.Windows.Media.Brushes.DarkMagenta;
+                            // maakt dit het laatste geselecteerde punt
+                            LastSelected = currentpoint;
+                        } else if (LastSelected.Y == currentpoint.Y)
+                        {
+                            // als het getal negatief is moet er naar links worden getekend, positief is rechts
+                            if ((LastSelected.Y - currentpoint.Y < 0))
+                            {
+                                // zolang het vorige coordinaat kleiner is
+                                for (int i = (int)LastSelected.X; i > currentpoint.X; i -= 25)
+                                {
+                                    LastSelected.X = i;
+                                    RectangleDictionary[LastSelected].Fill = System.Windows.Media.Brushes.DarkMagenta;
+                                }
+                            }
+                            else
+                            {
+                                for (int i = (int)LastSelected.X; i < currentpoint.X; i += 25)
+                                {
+                                    LastSelected.X = i;
+                                    RectangleDictionary[LastSelected].Fill = System.Windows.Media.Brushes.DarkMagenta;
+                                }
+                            }
+                            
+
+                            // voegt hoekje toe aan lijst
+                            SelectedPoints.Add(currentpoint);
+                            // maakt hokje magenta
+                            RectangleDictionary[currentpoint].Fill = System.Windows.Media.Brushes.DarkMagenta;
+                            // maakt dit het laatste geselecteerde punt
+                            LastSelected = currentpoint;
+                        }// als het niet gelijk is foei
+                        else
+                        {
+                            GeneralPopup warning = new GeneralPopup("sorry, je kunt niet schuin neerzetten");
+                            warning.Show();
+                        }
+
+
+                    }
+                    
                 }
                 else
                 {
-                    RectangleDictionary[new System.Windows.Point(y, x)].Fill = System.Windows.Media.Brushes.DarkMagenta;
-                    LastSelected = new System.Windows.Point(y, x);
+                    // kleurt het hokje in
+                    RectangleDictionary[currentpoint].Fill = System.Windows.Media.Brushes.DarkMagenta;
+                    // maakt dit het laatste geselecteerde punt
+                    LastSelected = currentpoint;
+                    // voegt het hokje toe aan de lijst
+                    SelectedPoints.Add(currentpoint);
 
                 }
+
+
 
 
             }
