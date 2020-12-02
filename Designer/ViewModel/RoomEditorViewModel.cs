bb025112f44b1e27046bfe9830ab82f3f6283895
Julien <julianvanrijckevorsel@gmail.com>
2020-12-02 16:13:59 +0100

    commentaar

--- Designer/ViewModel/RoomEditorViewModel.cs
+++ Designer/ViewModel/RoomEditorViewModel.cs
@@ -58,7 +58,7 @@ namespace Designer.ViewModel
         }
         public void SubmitRoom()
         {
-            var smallestposition =  SelectedPoints.Aggregate((p1, p2) => p1.X < p2.X || p1.Y < p2.Y ? p1 : p2);
+            var smallestposition =  SelectedPoints.Aggregate((p1, p2) => p1.X < p2.X || p1.Y < p2.Y ? p1 : p2); // Om te kijken welke van de punten de kleinste is
            List<Position> OffsetPositions = new List<Position>();
             foreach (var position in SelectedPoints)
             {
@@ -80,24 +80,23 @@ namespace Designer.ViewModel
         public void DrawGrid()
         {
 
-            var y = 25 * 25;
-            var x = 50 * 25;
+            var y = 25 * 25; // Scherm is 25 vakjes hoog
+            var x = 50 * 25; // Scherm is 50 vakjes breed
             for (int row = 0; row < y; row += 25)
-            {
-
+            { // Voor elke rij
                 for (int column = 0; column < x; column += 25)
-                {
+                { // In deze rij voor elke kolom
                     System.Windows.Shapes.Rectangle rectangle = new System.Windows.Shapes.Rectangle();
                     rectangle.Fill = System.Windows.Media.Brushes.White;
                     rectangle.Width = 25;
                     rectangle.Height = 25;
-                    rectangle.Stroke = System.Windows.Media.Brushes.Black;
+                    rectangle.Stroke = System.Windows.Media.Brushes.Black; // Maak vierkant
                     Canvas.SetTop(rectangle, row);
                     Canvas.SetLeft(rectangle, column);
-                    Position Point = new Position(column, row);
+                    Position Point = new Position(column, row); // Op de juiste plek
                     Points.Add(Point);
                     Editor.Children.Add(rectangle);
-                    RectangleDictionary.Add(Point, rectangle);
+                    RectangleDictionary.Add(Point, rectangle); // Maak hem aan
                 }
             }
         }
