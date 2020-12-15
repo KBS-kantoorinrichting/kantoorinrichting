851a5b2e702deb94b93f26d4d92eef009735bb2f
UnderKoen <koenvanstaveren@hotmail.com>
2020-12-15 15:01:54 +0100

    Test fix

--- Services/RoomService.cs
+++ Services/RoomService.cs
@@ -1,22 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using Models;
-using System.Linq;
 
 namespace Services {
     public class RoomService : BasicService<Room> {
         private static RoomService _instance;
-
-        public RoomService()
-        {
-            RoomDesignContext.Instance.RoomPlacements.ToList();
-        }
 
         public static RoomService Instance {
             get => _instance ??= new RoomService();
             set => _instance = value;
         }
 
-        protected override DbSet<Room> DbSet => RoomDesignContext.Instance.Rooms;
+        protected override DbSet<Room> DbSet {
+            get {
+                RoomDesignContext.Instance.RoomPlacements.ToList();
+                return RoomDesignContext.Instance.Rooms;
+            }
+        }
+
         protected override DbContext DbContext => RoomDesignContext.Instance;
     }
 }
