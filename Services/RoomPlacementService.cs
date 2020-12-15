acba438d14d0e9370dfdbc55e2bf690649b006cf
Kasper Vegte, van der <s1148087@student.windesheim.nl>
2020-12-15 14:44:45 +0100

    added roomplacement service

---
+++ Services/RoomPlacementService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+;
+
+namespace Services
+{
+    public class RoomPlacementService : BasicService<RoomPlacement>
+    {
+        private static RoomPlacementService _instance;
+
+        public static RoomPlacementService Instance
+        {
+            get => _instance ??= new RoomPlacementService();
+            set => _instance = value;
+        }
+
+        protected override DbSet<RoomPlacement> DbSet => RoomDesignContext.Instance.RoomPlacements;
+        protected override DbContext DbContext => RoomDesignContext.Instance;
+    }
+}
